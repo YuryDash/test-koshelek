77519207d2ecfbefb50d31e1b0c32e2e5aa15e68
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2024-06-26 16:27:31 +0300

    fin

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,5 @@ import {Provider} from "react-redux";
 import {store} from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <Provider store={store}><App/></Provider>
-  </React.StrictMode>,
 )
